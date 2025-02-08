7531e20efe8b0b97db24c80f51fb9e4b6ff7a4ec
Romuald <phyvavy@gmail.com>
2025-02-08 11:45:12 +0100

    filtres

---
+++ src/components/Filter.tsx
@@ -0,0 +1,52 @@
+const Filter = () => {
+  return (
+    <div className="mt-12 flex justify-between">
+      <div className="flex gap-6">
+        <select
+          name="type"
+          id=""
+          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+        >
+          <option>Type</option>
+          <option value="physical">Physical</option>
+          <option value="digital">Digital</option>
+        </select>
+        <select
+          name="size"
+          id=""
+          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+        >
+          <option>Size</option>
+          <option value="">Size</option>
+        </select>
+        <select
+          name="color"
+          id=""
+          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+        >
+          <option>Color</option>
+          <option value="">Test</option>
+        </select>
+        <select
+          name="ribbon"
+          id=""
+          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+        >
+          <option>Category</option>
+          <option value="">New Arrival</option>
+          <option value="">Popular</option>
+        </select>
+        <select
+          name=""
+          id=""
+          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+        >
+          <option>All Filters</option>
+        </select>
+      </div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Filter;
